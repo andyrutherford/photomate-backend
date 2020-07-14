9abf26be69f55e626e861b89bb4640ffdecd0edc
Andy <andrew.rutherford.6@gmail.com>
2020-07-14 09:09:08 +0700

    created server and established connection to db

---
+++ server.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+dotenv.config({ path: './config/config.env' });
+
+connectDB();
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// HTTP request logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
