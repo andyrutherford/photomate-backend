6776948870a25068fa79cc798c26be04eeff8654
Andy <andrew.rutherford.6@gmail.com>
2020-07-15 09:10:07 +0700

    added auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = function (req, res, next) {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  // Check if not token
+  if (!token) {
+    console.log('(auth middleware) - no token');
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+
+  // Verify token
+  try {
+    jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+      if (error) {
+        return res.status(401).json({ msg: 'Token is not valid' });
+      } else {
+        req.user = decoded.user;
+        next();
+      }
+    });
+  } catch (err) {
+    console.error('something wrong with auth middleware');
+    res.status(500).json({ msg: 'Server Error' });
+  }
+};
