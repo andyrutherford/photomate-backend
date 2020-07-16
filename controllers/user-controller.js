4ae9c320e2b9fbcb269c6621aca01cc75cb5b9f0
Andy <andrew.rutherford.6@gmail.com>
2020-07-16 09:40:59 +0700

    created updateProfile controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,12 +1,33 @@
 const User = require('../models/User');
 
 // @desc    Get user profile
-// @route   GET /api/v1/auth
+// @route   GET /api/v1/user
 // @access  PRIVATE
 exports.getProfile = async (req, res, next) => {
   try {
     const user = await User.findById(req.user.id).select('-password');
     res.status(200).json(user);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+// @desc    Update user profile
+// @route   GET /api/v1/user
+// @access  PRIVATE
+exports.updateProfile = async (req, res, next) => {
+  const { website, bio, phoneNumber, gender } = req.body;
+  const profile = {
+    website: website || '',
+    bio: bio || '',
+    phoneNumber: phoneNumber || null,
+    gender: gender || '',
+  };
+  try {
+    let user = await User.findById(req.user.id);
+    user.profile = profile;
+    await user.save();
+    res.send(user);
   } catch (error) {
     console.log(error.message);
   }
--- routes/user-route.js
+++ routes/user-route.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require('../middleware/auth');
-const { getProfile } = require('../controllers/user-controller');
+const { getProfile, updateProfile } = require('../controllers/user-controller');
 
 router.route('/').get(auth, getProfile);
+router.route('/').put(auth, updateProfile);
 
 module.exports = router;
