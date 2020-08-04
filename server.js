1ae833e0a8868e7f9d2b6566caf06c82cddbe93b
Andy <andrew.rutherford.6@gmail.com>
2020-08-04 11:12:37 +0700

    modify error handler

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
   }
-  res.status(error.code || 500).json({
+  res.status(500).json({
     success: error.success,
     message: error.message || 'An unknown error occurred.',
   });
